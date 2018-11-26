8dd5e2e076a1051404bb0d0b83df72e30b608655
Ziyao Zhang <ziyaointl@gmail.com>
2018-11-26 01:42:36 -0800

    Vector arithmetic

---
+++ main.py
@@ -0,0 +1,26 @@
+# Vector arithmetic
+
+def vector(x, y, z):
+    return [x, y, z]
+
+def add(v1, v2):
+    return [v1[i] + v2[i] for i in range(3)]
+
+def sub(v1, v2):
+    return [v1[i] - v2[i] for i in range(3)]
+
+def mul(v, x):
+    return [v[i] * x for i in range(3)]
+
+def dot(v1, v2):
+    return sum([v1[i] * v2[i] for i in range(3)])
+
+def norm(v):
+    return dot(v, v)**0.5
+
+def normalize(v):
+    return mul(v, 1/norm(v))
+
+def cross(v1, v2):
+    return vector(v1[1] * v2[2] - v1[2] * v2[1], v1[2] * v2[0] - v1[0] * v2[2], v1[0] * v2[1] - v1[1] * v2[0])
+
