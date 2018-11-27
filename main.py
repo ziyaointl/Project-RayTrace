ba193d80ced1d1c7b52e7bd0a40beb424a065382
Ziyao Zhang <ziyaointl@gmail.com>
2018-11-26 17:30:33 -0800

    Diffuse material

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import random
 from turtle import *
 from tkinter import *
-from math import sin, sqrt
+from math import sin, sqrt, pi, cos
 
 # Vector arithmetic
 
@@ -50,6 +50,14 @@ def norm(v):
     6.0
     """
     return dot(v, v)**0.5
+
+def weight(v, w):
+    """
+    >>> v1, v2 = vector(1, 2, 3), vector(10, 5, 6)
+    >>> weight(v1, v2)
+    [10, 10, 18]
+    """
+    return [v[i] * w[i] for i in range(3)]
 
 def normalize(v):
     return mul(v, 1/norm(v))
@@ -114,6 +122,7 @@ MAGIC_NUMBER = 0.5135 # fov?
 RAY_OFFSET = 140
 CAMERA_POS = vector(50,52,295.6)
 CAMERA_DIR = normalize(vector(0,-0.042612,-1))
+MAX_DEPTH = 10
 CX = vector(WIDTH * MAGIC_NUMBER / HEIGHT)
 CY = mul(normalize(cross(CX, CAMERA_DIR)), MAGIC_NUMBER)
 BLACK = vector()
@@ -180,8 +189,8 @@ def render():
     """
     Produces the image
     """
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
+    for y in range(100, HEIGHT - 100):
+        for x in range(100, WIDTH - 100):
             print(x, y)
             color = vector()
             for sy in range(SUBSAMPLES):
@@ -197,15 +206,82 @@ def render():
                         o = add(CAMERA_POS, mul(d, RAY_OFFSET))
                         d = normalize(d)
                         r = add(r, mul(radiance(ray(o, d), 0), 1/SAMPLES))
+                    r = vector(clamp(r[0]), clamp(r[1]), clamp(r[2]))
                     color = add(color, mul(r, 1/SUBSAMPLES**2)) # need to clamp r before adding
             pixel(x, y, color)
 
+def orthonormal_basis(w):
+    """
+    Takes in a vector w
+    Returns a tuple of three vectors w, u, v orthonormal to each other
+    """
+    u = normalize(cross(vector(0.0, 1.0) if abs(w[0]) > 0.1 else vector(1.0), w))
+    v = cross(w, u)
+    return w, u, v
+
+def clamp(x):
+    if x < 0:
+        return 0
+    if x > 1:
+        return 1
+    return x
 
 def radiance(r, depth, emissive=True):
     t, i = intersect_scene(r, spheres)
-    if (i == -1):
+    if i == -1 or depth > 10:
         return BLACK
-    return color(spheres[i])
+    # Constant shading: for testing purposes only
+    # return color(spheres[i])
+    s = spheres[i]
+    intersection_p = add(origin(r), mul(direction(r), t))
+    sphere_normal = normalize(sub(intersection_p, center(s)))
+    # nl: Properly oriented version of sphere_normal
+    nl = sphere_normal if dot(sphere_normal, direction(r)) < 0 else mul(sphere_normal, -1)
+    # Russian Roulette
+    surface_color = color(s)
+    p = max(surface_color)
+    if depth > 5 or p == 0.0:
+        if (random() < p): # continue
+            surface_color = mul(surface_color, 1 / p)
+        else:
+            return mul(emission(s), int(emissive))
+    if reflection(s) == 'DIFF':
+        r1 = 2 * pi * random() # random angle
+        r2 = random()
+        r2s = sqrt(r2) # random distance
+        # Create orthonormal coordinate frame
+        w, u, v = orthonormal_basis(nl.copy())
+        # Create random direction
+        d1 = mul(mul(u, cos(r1)), r2s)
+        d2 = mul(mul(v, sin(r1)), r2s)
+        d3 = mul(w, sqrt(1 - r2))
+        d = normalize(add(d1, add(d2, d3)))
+
+        # Sample lights
+        e = vector()
+        for l in spheres:
+            if max(emission(l)) > 0:
+                # Create random direction towards the light
+                sw, su, sv = orthonormal_basis(sub(center(l), intersection_p))
+                x_sp = sub(intersection_p, center(l))
+                cos_a_max = sqrt(1 - (radius(l)**2 / dot(x_sp, x_sp)))
+                eps1, eps2 = random(), random()
+                cos_a = 1 - eps1 + eps1 * cos_a_max
+                sin_a = sqrt(1 - cos_a**2)
+                phi = 2 * pi * eps2
+                
+                l1 = mul(mul(su, cos(phi)), sin_a)
+                l2 = mul(mul(sv, sin(phi)), sin_a)
+                l3 = mul(sw, cos_a)
+                dl = normalize(add(l1, add(l2, l3)))
+
+                # Shadow ray
+                shadow_t, shadow_i = intersect_scene(ray(intersection_p, dl), spheres)
+                if shadow_i != -1 and spheres[shadow_i] is l:
+                    omega = 2 * pi * (1 - cos_a_max)
+                    e = add(e, mul(weight(surface_color, mul(emission(l), dot(dl, nl) * omega)), 1/pi))
+        return add(add(emission(s), e), radiance(ray(intersection_p, d), depth - 1, 0))
+    return color(s)
 
 render()
 exitonclick()
