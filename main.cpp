3a84ce6671b62a7c217fba9bed975329a2f998a8
Ziyao Zhang <ziyaointl@gmail.com>
2019-09-14 22:10:15 -0700

    Add world

--- main.cpp
+++ main.cpp
@@ -10,12 +10,9 @@
 #define WIDTH 400
 #define HEIGHT 200
 
-Vec3 color(const Ray &r) {
+Vec3 color(const Ray &r, const Hittable &world) {
     HitRecord rec;
-    HittableList hList;
-    Sphere s = Sphere(Vec3(0, 0, -1), 0.5);
-    hList.hittables.push_back(&s);
-    if (hList.hit(r, 0.0, std::numeric_limits<float>::max(), rec)) {
+    if (world.hit(r, 0.0, std::numeric_limits<float>::max(), rec)) {
         return (rec.normal + Vec3(1, 1, 1)) * 0.5; // Scale colors
     }
     Vec3 unit_d = normalized(r.direction);
@@ -31,12 +28,19 @@ int main() {
     const Vec3 vertical = Vec3(0, 2, 0);
     const Vec3 lowerLeft = Vec3(-2, -1, -1);
 
+    // Initialize world
+    HittableList hList;
+    Sphere s1 = Sphere(Vec3(0, 0, -1), 0.5); // Sphere in the center
+    Sphere s2 = Sphere(Vec3(0, -101, -1), 100);
+    hList.hittables.push_back(&s1);
+    hList.hittables.push_back(&s2);
+
     for (int y = HEIGHT - 1; y >= 0; --y) {
         for (int x = 0; x < WIDTH; ++x) {
             float u = float(x) / WIDTH;
             float v = float(y) / HEIGHT;
             Ray r = Ray(origin, lowerLeft + u * horizontal + v * vertical);
-            Vec3 pixColor = color(r) * 255.99;
+            Vec3 pixColor = color(r, hList) * 255.99;
             std::cout << int(pixColor.r()) << " " << int(pixColor.g()) << " " << int(pixColor.b()) << std::endl;
         }
     }
