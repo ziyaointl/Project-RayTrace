433110f8ce2431a3c21356c3fd93dabcb8056639
Ziyao Zhang <ziyaointl@gmail.com>
2019-09-14 20:09:00 -0700

    Add more vector capabilities

--- vec3.hpp
+++ vec3.hpp
@@ -42,6 +42,20 @@ class Vec3 {
             }
             return *this;
         }
+        
+        float operator*(const Vec3 &rhs) const {
+            return x()*rhs.x() + y()*rhs.y() + z()*rhs.z();
+        }
+
+        Vec3 operator+(const Vec3 &rhs) const {
+            Vec3 sum = *this;
+            sum += rhs;
+            return sum;
+        }
+
+        Vec3 operator-(const Vec3 &rhs) const {
+            return *this + (-rhs);
+        }
 
         Vec3 operator-() const {
             return Vec3(-elements[0], -elements[1], -elements[2]);
@@ -72,12 +86,6 @@ class Vec3 {
         float elements[3] = {0.0, 0.0, 0.0};
 };
 
-Vec3 operator+ (const Vec3 &lhs, const Vec3 &rhs) {
-    Vec3 sum = lhs;
-    sum += rhs;
-    return sum;
-}
-
 Vec3 normalized(const Vec3 v) {
     float l = v.length();
     return v / l;
@@ -90,5 +98,6 @@ Vec3 operator* (const Vec3 &lhs, const float &rhs) {
 Vec3 operator* (const float &lhs, const Vec3 &rhs) {
     return rhs * lhs;
 }
+
 
 #endif
